603ba66bf315785b2f9902b0e5f544184249cf0a
lubos.ilcik <lubos.ilcik@touch4it.com>
2018-10-25 19:38:14 +0200

    init

---
+++ AutoLayoutDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutDemo
+//
+//  Created by Lubos Ilcik on 25/10/2018.
+//  Copyright © 2018 Touch4It s.r.o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
